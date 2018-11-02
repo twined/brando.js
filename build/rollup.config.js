561e08d2356b92da2252c8d7f8666aacf8beca3e
T. Mjoen <trond@twined.net>
2018-11-02 10:53:54 +0100

    Pre rollup up

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,25 +1,25 @@
-const fs = require("fs");
-const path = require("path");
-const vue = require("rollup-plugin-vue");
-const alias = require("rollup-plugin-alias");
-const babel = require("rollup-plugin-babel");
-const scss = require("rollup-plugin-scss");
-const json = require("rollup-plugin-json");
-const graphql = require("rollup-plugin-graphql");
-const resolve = require("rollup-plugin-node-resolve");
-const commonjs = require("rollup-plugin-commonjs");
-const CleanCSS = require("clean-css");
-const { camelCase } = require("lodash");
-const { name, dependencies } = require("../package.json");
+const fs = require('fs')
+const path = require('path')
+const vue = require('rollup-plugin-vue')
+const alias = require('rollup-plugin-alias')
+const babel = require('rollup-plugin-babel')
+const scss = require('rollup-plugin-scss')
+const json = require('rollup-plugin-json')
+const graphql = require('rollup-plugin-graphql')
+const resolve = require('rollup-plugin-node-resolve')
+const commonjs = require('rollup-plugin-commonjs')
+const CleanCSS = require('clean-css')
+const { camelCase } = require('lodash')
+const { name, dependencies } = require('../package.json')
 
-const base = path.resolve(__dirname, "..");
-const nodeModules = path.resolve(base, "node_modules");
-const lib = path.resolve(base, "lib");
-const dist = path.resolve(base, "dist");
+const base = path.resolve(__dirname, '..')
+const nodeModules = path.resolve(base, 'node_modules')
+const lib = path.resolve(base, 'lib')
+const dist = path.resolve(base, 'dist')
 
 // Ensure dist directory exists
 if (!fs.existsSync(dist)) {
-    fs.mkdirSync(dist);
+  fs.mkdirSync(dist)
 }
 
 // Libs in `external` will not be bundled to dist,
@@ -27,49 +27,51 @@ if (!fs.existsSync(dist)) {
 // We want to include some of them in the build, so we exclude it here.
 
 export default {
-    input: path.resolve(lib, "index.js"),
-    // Libs in `external` will not be bundled to dist,
-    // since they are expected to be provided later.
-    external: [ 
-      'moment-timezone', 
-      'url-slug', 
-      'apollo-client', 
-      'apollo-absinthe-upload-client', 
-      'nprogress', 
-      'villain-editor',
-      '@jacobmischka/vue-flatpickr', 
-      'vuex', 
-      'vue' 
-    ],
-    plugins: [
-      alias({
-        '@': lib,
-        '~': nodeModules
-      }),
-      scss({
-        output: path.resolve(dist, name + ".css"),
-        insert: true,
-		    include: '**/*.scss',
-		    exclude: [],
-        includePaths: ['node_modules']
-      }),
-      graphql({ sourceMap: true }),
-      vue(),
-      resolve({
-        jsnext: true,
-        main: true
-      }),
-      json(),
-      commonjs(),
-      babel()
-    ],
+  input: path.resolve(lib, 'index.js'),
+  // Libs in `external` will not be bundled to dist,
+  // since they are expected to be provided later.
+  external: [
+    'moment-timezone',
+    'url-slug',
+    'apollo-client',
+    'apollo-absinthe-upload-client',
+    'nprogress',
+    'vex-dialog',
+    'vex-js',
+    'villain-editor',
+    '@jacobmischka/vue-flatpickr',
+    'vuex',
+    'vue'
+  ],
+  plugins: [
+    alias({
+      '@': lib,
+      '~': nodeModules
+    }),
+    scss({
+      output: path.resolve(dist, name + '.css'),
+      insert: true,
+      include: '**/*.scss',
+      exclude: [],
+      includePaths: ['node_modules']
+    }),
+    graphql({ sourceMap: true }),
+    vue(),
+    resolve({
+      jsnext: true,
+      main: true
+    }),
+    json(),
+    commonjs(),
+    babel()
+  ],
 
-    output: [
-      {
-        name,
-        format: "es",
-        file: path.resolve(dist, name + ".esm.js"),
-        sourcemap: true
-      }
-    ]
-};
+  output: [
+    {
+      name,
+      format: 'es',
+      file: path.resolve(dist, name + '.esm.js'),
+      sourcemap: true
+    }
+  ]
+}
